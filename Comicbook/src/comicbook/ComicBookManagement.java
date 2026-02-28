1552b013a653a842a781fc28b0deab479ff1869d
bale178 <133573564+bale178@users.noreply.github.com>
2026-02-28 21:00:20 +0700

    Refactor ComicBookManagement class structure

--- Comicbook/src/comicbook/ComicBookManagement.java
+++ Comicbook/src/comicbook/ComicBookManagement.java
@@ -1,4 +1,4 @@
-/*
+/*qqqqqqqqq
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -534,3 +534,4 @@ public class ComicBookManagement {
         return out;
     }
 }
+
