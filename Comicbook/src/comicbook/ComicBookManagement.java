0cc4ffe23af6aacb709289ae1bc3d237ae1fd1cf
baongce181286-boop <baongce181286@fpt.edu.vn>
2026-02-28 21:12:45 +0700

    Update ComicBookManagement.java

--- Comicbook/src/comicbook/ComicBookManagement.java
+++ Comicbook/src/comicbook/ComicBookManagement.java
@@ -1,4 +1,4 @@
-/* e32efqf2qf
+/* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -534,6 +534,7 @@ public class ComicBookManagement {
         return out;
     }
 }
+
 
 
 
